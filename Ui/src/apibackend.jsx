4d92c8208b6142303a6ffadbf10387670d28a41b
Manish kumar <mrjackel88@gmail.com>
2026-04-12 03:06:39 +0530

    change api

--- Ui/src/apibackend.jsx
+++ Ui/src/apibackend.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function apibackend() {
-   const API  = "https://vega-persional-ai-rht2-li16t2rc1.vercel.app";
+   const API  = "https://vega-persional-ai.onrender.com";
   
 }
 

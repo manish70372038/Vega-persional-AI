8451869bd434160d3ac330729f15d65177caaa16
Manish kumar <mrjackel88@gmail.com>
2026-04-30 16:48:35 +0530

    updated url and promt

--- Ui/src/apibackend.jsx
+++ Ui/src/apibackend.jsx
@@ -1,3 +1,4 @@
 import React from 'react'
-   const apibackend="https://vega-persional-ai.onrender.com";
+   const apibackend="https://vega-persional-ai-1.onrender.com";
+//    vega-persional-ai-aueb-nyd8qrc98.vercel.app
 export default apibackend
